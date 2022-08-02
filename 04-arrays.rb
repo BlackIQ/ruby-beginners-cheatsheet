e9adca118acf0defd82f9b468dc5fb52494ef4df
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-02 14:15:57 +0430

    Add running for n time

--- 04-arrays.rb
+++ 04-arrays.rb
@@ -9,3 +9,25 @@ names.each do | name |
   # Print name
   puts name
 end
+
+puts "- - - - -"
+
+# Create a JSON array
+person = {
+  "name" => "Amir",
+  "age" => 19,
+  "born" => "Nov 20 2003",
+  "city" => "Tehran",
+}
+
+# For loop in person with key and value
+person.each do |key, value|
+  # Print key and value
+  puts "#{key} is #{value}"
+end
+
+puts "- - - - -"
+
+(0..10).each do |i|
+  puts "Running for #{i} time"
+end

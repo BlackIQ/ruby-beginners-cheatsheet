81b3d9901e0da3ca7c2f4cab3b7451c7cb0b6b9e
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-02 14:09:51 +0430

    Add a sample list and loop in it

---
+++ 04-arrays.rb
@@ -0,0 +1,11 @@
+# Ruby cheatsheet number 4
+# Arrays
+
+# Create just a list
+names = [ "Amir", "Fatemeh", "Mohammad", "Ali" ]
+
+# For loop in a list
+names.each do | name |
+  # Print name
+  puts name
+end
